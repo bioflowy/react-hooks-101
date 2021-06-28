43043f5144f2bf1e7bf453e9daeb9d961f044db7
Uehara.Keizo <Uehara.Keizo@mss.co.jp>
2021-06-28 05:23:41 +0000

    my first react state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState } from 'react';
 
 const App = () => {
-  return (
-    <div >
-      This is template for React App
-    </div>
+  const [count, setCount ] = useState(0)
+  const increment = () => setCount(count+1)
+  const decrement = () => setCount(count-1)
+  const increment2 = () => setCount(count => count+1)
+  const decrement2 = () => setCount(count => count-1)
+  const multi2 = () => setCount(count => count*2)
+  const reset = () => setCount(0)
+  const div3 = () => setCount(count => count % 3 === 0 ? count/3:count)
+    return (
+    <>
+    <div >count: {count}</div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <button onClick={increment2}>+1</button>
+    <button onClick={decrement2}>-1</button>
+    <button onClick={reset}>Reset</button>
+    <button onClick={multi2}>*2</button>
+    <button onClick={div3}>3の倍数の時だけ3で割る</button>
+    </>
   );
 }
 
